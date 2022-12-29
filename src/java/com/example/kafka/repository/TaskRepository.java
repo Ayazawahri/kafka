893c3a9db4baf359dddd95e66df8c2afb8fa18aa
Ayazawahri <114745947+Ayazawahri@users.noreply.github.com>
2022-12-30 00:17:01 +0100

    Delete src/java directory

--- src/java/com/example/kafka/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.kafka.repository;
-
-import com.example.kafka.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-}
--- src/java/com/example/kafka/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.kafka.repository;
-
-import com.example.kafka.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findUserById(int id);
-}
